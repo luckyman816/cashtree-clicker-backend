e687f40ced09188a230f9e21dc258938085c833e
Senior Coder <agilecoder2040@gmail.com>
2024-08-08 17:38:44 +0900

    d

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoURI: "mongodb://localhost:27017/cashtree-telegram-game",
+  // mongoURI: "mongodb://localhost:27017/cashtree-telegram-game",
+  mongoURI: "mongodb+srv://walter:nEObatlD6OcCVGaF@walter.iiya7fr.mongodb.net/cashtree-telegram-game",
   jwtSecret: "kut10developers",
   githubToken: "<yoursecrectaccesstoken>",
 };
